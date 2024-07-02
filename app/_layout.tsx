4b8b28f9ac76b709b81086af2bae97f526254d93
Chris Kang <chris.v.kang@gmail.com>
2024-07-02 00:28:17 -0500

    feat: the bird falls

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
+      <GestureHandlerRootView>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
-import React, { useEffect } from "react";
-import { Canvas, Group, useImage, Image } from "@shopify/react-native-skia";
+import React, { useEffect, useState } from "react";
+import {
+  Canvas,
+  Group,
+  useImage,
+  Image,
+  scale,
+} from "@shopify/react-native-skia";
 import { useWindowDimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
@@ -8,7 +14,11 @@ import {
   withTiming,
   withSequence,
   withRepeat,
+  useFrameCallback,
 } from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+
+const GRAVITY = 9.8;
 
 const App = () => {
   const { width, height } = useWindowDimensions();
@@ -18,16 +28,32 @@ const App = () => {
   const pipe = useImage(require("@/assets/sprites/pipe-green.png"));
   const ground = useImage(require("@/assets/sprites/base.png"));
 
+  const pipeWidth = 104;
+  const pipeHeight = 640;
   const pipeOffset = height / 1.6;
   const pipeOpening = 250;
 
-  const x = useSharedValue(width - 50);
+  const x = useSharedValue(width);
 
+  const groundX = useSharedValue(0);
+  const birdY = useSharedValue(height / 2.4);
+  const birdYVelocity = useSharedValue(0);
+
+  const touch = Gesture.Tap()
+    .onTouchesDown(() => {})
+    .onEnd(() => {});
+
+  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+    if (dt === null) return;
+    birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt) / 1000;
+    birdY.value = birdY.value + birdYVelocity.value;
+  });
+
+  // Animate Ground
   useEffect(() => {
-    x.value = withRepeat(
+    groundX.value = withRepeat(
       withSequence(
-        withTiming(-104, { duration: 3000, easing: Easing.linear }),
-        withTiming(width, { duration: 0 })
+        withTiming(-width, { duration: 2000, easing: Easing.linear })
       ),
       -1
     );
@@ -35,44 +61,53 @@ const App = () => {
 
   return (
     <SafeAreaView>
-      <Canvas style={{ width, height, backgroundColor: "red" }}>
-        <Image image={bg} height={height} width={width} fit={"cover"} />
+      <GestureDetector gesture={touch}>
+        <Canvas style={{ width, height, backgroundColor: "red" }}>
+          {/* Background */}
+          <Image image={bg} height={height} width={width} fit={"cover"} />
 
-        <Group>
+          {/* Pipes */}
+          <Group>
+            <Image
+              image={pipe}
+              height={pipeHeight}
+              width={pipeWidth}
+              x={x}
+              y={-height + pipeOffset}
+              transform={[{ rotate: Math.PI }, { scaleX: -1 }]}
+              origin={{ x: width / 2, y: 0 }}
+            />
+
+            <Image
+              image={pipe}
+              height={pipeHeight}
+              width={pipeWidth}
+              x={x}
+              y={height - pipeOffset + pipeOpening}
+            />
+          </Group>
+
+          {/* Ground */}
           <Image
-            image={pipe}
-            height={640}
-            width={104}
-            x={x}
-            y={-height + pipeOffset}
-            transform={[{ rotate: Math.PI }, { scaleX: -1 }]}
-            origin={{ x: width / 2, y: 0 }}
+            image={ground}
+            height={width / 3}
+            width={width}
+            x={groundX}
+            y={height - width / 3}
+          />
+          <Image
+            image={ground}
+            height={width / 3}
+            width={width}
+            x={groundX}
+            y={height - width / 3}
+            transform={[{ translateX: width }]}
           />
 
-          <Image
-            image={pipe}
-            height={640}
-            width={104}
-            x={x}
-            y={height - pipeOffset + pipeOpening}
-          />
-        </Group>
-
-        <Image
-          image={ground}
-          height={width / 3}
-          width={width}
-          y={height - width / 3}
-        />
-
-        <Image
-          image={bird}
-          width={64}
-          height={48}
-          y={height / 2}
-          x={width / 4}
-        />
-      </Canvas>
+          {/* Bird */}
+          <Image image={bird} width={64} height={48} y={birdY} x={width / 4} />
+        </Canvas>
+      </GestureDetector>
     </SafeAreaView>
   );
 };
